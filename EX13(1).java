21bc7325415994183da1133c7e8a073ee267720d
RINI AMADO <74702205+riniamado@users.noreply.github.com>
2022-03-07 12:40:49 +0530

    Create EX13(1).java

---
+++ EX13(1).java
@@ -0,0 +1,17 @@
+package com.example.exercise_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
+    }
+}
