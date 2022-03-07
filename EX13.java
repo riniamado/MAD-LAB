ddc8d103d0a95966925b32126b4b31369b5930eb
RINI AMADO <74702205+riniamado@users.noreply.github.com>
2022-03-07 12:39:31 +0530

    Create EX13.java

---
+++ EX13.java
@@ -0,0 +1,26 @@
+package com.example.exercise_13;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
