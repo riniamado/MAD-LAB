a45fbef698f0b7838855a6a41b000ffd957809fd
RINI AMADO <74702205+riniamado@users.noreply.github.com>
2022-03-07 12:46:10 +0530

    Create EX16.java

---
+++ EX16.java
@@ -0,0 +1,31 @@
+package com.example.exercise_16;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b=(Button)findViewById(R.id.clickme);
+        TextView t=(TextView)findViewById(R.id.txt);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(i);
+            }
+        });
+        Bundle bundle=getIntent().getExtras();
+        String msg=bundle.getString("msg");
+        if(msg!=null)
+            t.setText(msg);
+
+    }
+}
