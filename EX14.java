93582869d4fd3b5e97d75544e7a1d4b05c074075
RINI AMADO <74702205+riniamado@users.noreply.github.com>
2022-03-07 12:42:10 +0530

    Create EX14.java

---
+++ EX14.java
@@ -0,0 +1,27 @@
+package com.example.exercise_14;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button1=(Button)findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Main2Activity.class);
+                i.putExtra("Message","Welcome to Android application");
+                startActivity(i);
+            }
+        });
+    }
+}
